680e4d2590479d68cef9d8a0939bcded24fa6eb5
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2023-12-09 22:07:51 +0400

    Second lecture

--- BCTSO-20-NC/FirstConsoleApp/Program.cs
+++ BCTSO-20-NC/FirstConsoleApp/Program.cs
@@ -2,9 +2,124 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello I am nika");
+            #region ლექცია 2
+
+
+            //IMPLICT CAST - არაცხადი ტიპიზაცია
+            //byte y = 10;
+            //int x = y;
+
+
+            //EXPLICIT CAST -ცხადი ტიპიზაცია
+            //Arithemmitac overflow - არითმეტიკული გადავსება
+            //int x = 256;
+            //byte y = checked((byte)x);
+
+
+
+
+            // + - * / %
+            // > < >= <= == != // bool
+            //++ -- += -= *= /=
+            // || ან  && და
+
+
+            //string x = "A";
+            //string y = "B";
+
+            //string z = x + y;
+
+            //int x = 15;
+            //int y = 16;
+
+            //x += y;
+            //x -= y;
+            //x *= y;
+            //x /= y;
+
+            //x = x + y;
+            //x = x - y;
+            //x = x * y;
+            //x = x / y;
+
+
+            //int number = 0;
+            //if (number > 0)
+            //{
+            //    Console.WriteLine("POSITIVE");
+            //}
+            //else if (number == 0)
+            //{
+            //    Console.WriteLine("ZERO");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("NEGATIVE");
+            //}
+
+
+            //int number = 60;
+
+            //if (number == 2 && number % 2 == 1)
+            //{
+            //    Console.WriteLine("number is Less then 5");
+            //}
+            //else if (number == 10)
+            //{
+            //    Console.WriteLine("number is TEN");
+            //}
+            //else if (number == 30)
+            //{
+            //    Console.WriteLine("number is THIRTEE");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("TEST");
+            //}
+
+            //switch (number)
+            //{
+            //    case 0:
+            //    case 1:
+            //    case 2:
+            //    case 3:
+            //    case 4:
+            //        Console.WriteLine("number is Less then 5");
+            //        break;
+            //    case 10:
+            //        Console.WriteLine("number is TEN");
+            //        break;
+            //    case 30:
+            //        Console.WriteLine("number is THIRTEE");
+            //        break;
+            //    default:
+            //        Console.WriteLine("TEST");
+            //        break;
+            //}
+
+
+            //Console.Write("Enter the first number: ");
+            //int firstNumber = int.Parse(Console.ReadLine());
+
+            //Console.Write("Enter the second number: ");
+            //int secondNumber = int.Parse(Console.ReadLine());
+
+            //Console.Write("Enter the third number: ");
+            //int thirdNumber = int.Parse(Console.ReadLine());
+
+            //Console.Write("Enter the fourth number: ");
+            //int fourthNumber = int.Parse(Console.ReadLine());
+
+
+            //Console.WriteLine(firstNumber + " " + secondNumber + " " + thirdNumber + " " + fourthNumber);
+            //Console.WriteLine($"{firstNumber}{secondNumber}{thirdNumber}{fourthNumber}");
+            //Console.WriteLine("{0}{1}{2}{3}",firstNumber,secondNumber,thirdNumber,fourthNumber);
+
+            #endregion
+
+
         }
     }
 }
