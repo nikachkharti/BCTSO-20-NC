32d65ba0afb930572daa0eb07c053e90554de8bf
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-01-13 21:32:28 +0400

    .

---
+++ BCTSO-20-NC/SecondConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SecondConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
