cad3d3559d333945f5e46848c2d038bb41f3e666
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-03-23 22:23:48 +0400

    Hotels project started

---
+++ BCTSO-20-NC/HotelProject.Data/DoitHotelBCTFOsql.sql
@@ -0,0 +1,15 @@
+﻿--CREATE DATABASE DOITHotel_BCTSO
+--DOITHotel_BCTSO
+
+--CREATE TABLE Managers
+--(
+--	Id INT PRIMARY KEY IDENTITY NOT NULL,
+--	FirstName NVARCHAR(50) NOT NULL,
+--	LastName NVARCHAR(50) NOT NULL
+--)
+
+--INSERT INTO Managers (FirstName,LastName)
+--VALUES
+--(N'გიორგი',N'გიორგაძე'),
+--(N'დავით',N'დავითაშვილი'),
+--(N'ალექსანრე',N'ჭავჭავაძე')
---
+++ BCTSO-20-NC/HotelProject.Tests/ManagersShould.cs
@@ -0,0 +1,33 @@
+﻿using HotelProject.Models;
+using HotelProject.Repository;
+
+namespace HotelProject.Tests
+{
+    public class ManagersShould
+    {
+        private readonly ManagerRepository _managerRepository;
+        public ManagersShould()
+        {
+            _managerRepository = new();
+        }
+
+
+        [Fact]
+        public void GetManagers()
+        {
+            var result = _managerRepository.GetManagers();
+        }
+
+        [Fact]
+        public void AddNewManager()
+        {
+            Manager newManager = new()
+            {
+                FirstName = "Test",
+                LastName = "Test"
+            };
+
+            _managerRepository.AddManager(newManager);
+        }
+    }
+}
