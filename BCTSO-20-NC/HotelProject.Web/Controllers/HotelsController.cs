aab6a9c6810d16ac085e125b368081557ebb5c64
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-04-04 21:16:17 +0400

    CRUD for hotels

--- BCTSO-20-NC/HotelProject.Web/Controllers/HotelsController.cs
+++ BCTSO-20-NC/HotelProject.Web/Controllers/HotelsController.cs
@@ -18,5 +18,47 @@ namespace HotelProject.Web.Controllers
             List<Hotel> hotels = await _hotelRepository.GetHotels();
             return View(hotels);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePOST(Hotel model)
+        {
+            await _hotelRepository.AddHotel(model);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _hotelRepository.GetSingleHotel(id);
+            return View(result);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> DeletePOST(int id)
+        {
+            await _hotelRepository.DeleteHotel(id);
+            return RedirectToAction("Index");
+        }
+
+
+        public async Task<IActionResult> Update(int id)
+        {
+            var result = await _hotelRepository.GetSingleHotel(id);
+            return View(result);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UpdatePOST(Hotel model)
+        {
+            await _hotelRepository.UpdateHotel(model);
+            return RedirectToAction("Index");
+        }
+
     }
 }
