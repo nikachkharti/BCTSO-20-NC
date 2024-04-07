9f9272b6487fc3edf8f272c36a2d168eab8cb1eb
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-04-07 15:32:50 +0400

    .

--- BCTSO-20-NC/HotelProject.Web/Controllers/ManagersController.cs
+++ BCTSO-20-NC/HotelProject.Web/Controllers/ManagersController.cs
@@ -1,21 +1,70 @@
 ﻿using HotelProject.Models;
 using HotelProject.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HotelProject.Web.Controllers
 {
     public class ManagersController : Controller
     {
         private readonly ManagerRepository _managerRepository;
-        public ManagersController()
+        private readonly HotelRepository _hotelRepository;
+        public ManagersController(HotelRepository hotelRepository, ManagerRepository managerRepository)
         {
-            _managerRepository = new ManagerRepository();
+            _managerRepository = managerRepository;
+            _hotelRepository = hotelRepository;
         }
 
         public async Task<IActionResult> Index()
         {
             List<Manager> managers = await _managerRepository.GetManagers();
             return View(managers);
+        }
+
+
+        //TODO GetHotelsWithoutManager method.
+        //public async Task<IActionResult> Create()
+        //{
+        //    var hotels = await _hotelRepository.GetHotelsWithoutManager();
+        //    ViewBag.HotelId = new SelectList(hotels, "Id", "Name");
+        //    return View();
+        //}
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Manager model)
+        {
+            await _managerRepository.AddManager(model);
+            return RedirectToAction("Index");
+        }
+
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _managerRepository.GetSingleManager(id);
+            return View(result);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> DeletePOST(int id)
+        {
+            await _managerRepository.DeleteManager(id);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            var result = await _managerRepository.GetSingleManager(id);
+            return View(result);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UpdatePOST(Manager model)
+        {
+            await _managerRepository.UpdateManager(model);
+            return RedirectToAction("Index");
         }
     }
 }
