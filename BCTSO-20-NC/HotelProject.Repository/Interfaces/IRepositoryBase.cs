8769e5c302a39012d9fefcc8e4f03ea547770b66
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-04-18 22:00:33 +0400

    Generic repositories added heads only

---
+++ BCTSO-20-NC/HotelProject.Repository/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace HotelProject.Repository.Interfaces
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        Task<List<T>> GetAllAsync(Func<T, bool> filter, string includeProperties = null);
+        Task<List<T>> GetAllAsync(string includeProperties = null);
+        Task<T> GetAsync(Func<T, bool> filter, string includeProperties = null);
+        Task AddAsync(T entity);
+        Task RemoveAsync(T entity);
+    }
+}
---
+++ BCTSO-20-NC/HotelProject.Repository/RepositoryBase.cs
@@ -0,0 +1,42 @@
+﻿using HotelProject.Data;
+using HotelProject.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelProject.Repository
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private DbSet<T> _dbSet;
+        public RepositoryBase(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAllAsync(Func<T, bool> filter, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>> GetAllAsync(string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(Func<T, bool> filter, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
