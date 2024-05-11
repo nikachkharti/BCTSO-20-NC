09c0a602e1916d206c7a9ef1fd4909a6908bd462
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-05-11 20:29:55 +0400

    Swagger authorization

--- BCTSO-20-NC/Todo.API/MiddlwareExtensions.cs
+++ BCTSO-20-NC/Todo.API/MiddlwareExtensions.cs
@@ -84,7 +84,30 @@ namespace Todo.API
 
         public static void AddSwagger(this WebApplicationBuilder builder)
         {
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme, securityScheme: new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Description = "Enter the Bearer Authorization string example: `Bearer Generated-JWT-Token`",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = JwtBearerDefaults.AuthenticationScheme
+                });
+
+                options.AddSecurityRequirement(
+                new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme,Id = JwtBearerDefaults.AuthenticationScheme}
+                        },
+                        new string [] {}
+                    }
+                });
+
+            });
         }
 
 
