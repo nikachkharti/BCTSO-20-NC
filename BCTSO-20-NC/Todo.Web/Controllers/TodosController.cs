c24880fb51f786545c02be1821745d1a02422d20
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-05-30 21:24:26 +0400

    Fronted MVC application added with base API service calls

---
+++ BCTSO-20-NC/Todo.Web/Controllers/TodosController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Todo.Web.Interfaces;
+
+namespace Todo.Web.Controllers
+{
+    public class TodosController : Controller
+    {
+        private readonly ITodoService _todoService;
+        public TodosController(ITodoService todoService)
+        {
+            _todoService = todoService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BCTSO-20-NC/Todo.Web/Services/BaseService.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using Todo.Web.Interfaces;
+using Todo.Web.Models;
+
+namespace Todo.Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<ApiResponse> SendAsync(ApiRequest request)
+        {
+            try
+            {
+                HttpClient client = _httpClientFactory.CreateClient("TodoAPI");
+                HttpRequestMessage message = new();
+                message.Headers.Add("Accept", "application/json");
+                //token
+
+                message.RequestUri = new Uri(request.Url);
+                if (request.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(request.Data), Encoding.UTF8, "application/json");
+                }
+
+                HttpResponseMessage apiResponse = new();
+
+                switch (request.ApiType)
+                {
+                    case ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case ApiType.PATCH:
+                        message.Method = HttpMethod.Patch;
+                        break;
+                    case ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case ApiType.DELETE:
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                apiResponse = await client.SendAsync(message);
+
+                switch (apiResponse.StatusCode)
+                {
+                    case System.Net.HttpStatusCode.NotFound:
+                        return new ApiResponse() { IsSuccess = false, Message = "Not Found" };
+                    case System.Net.HttpStatusCode.Forbidden:
+                        return new ApiResponse() { IsSuccess = false, Message = "Access denied" };
+                    case System.Net.HttpStatusCode.BadRequest:
+                        return new ApiResponse() { IsSuccess = false, Message = "Invalid parameter passed" };
+                    case System.Net.HttpStatusCode.Unauthorized:
+                        return new ApiResponse() { IsSuccess = false, Message = "Unauthorized" };
+                    case System.Net.HttpStatusCode.InternalServerError:
+                        return new ApiResponse() { IsSuccess = false, Message = "Internal server error" };
+                    default:
+                        var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<ApiResponse>(apiContent);
+                        return result;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ApiResponse result = new()
+                {
+                    Message = ex.Message,
+                    IsSuccess = false
+                };
+
+                return result;
+            }
+        }
+    }
+}
